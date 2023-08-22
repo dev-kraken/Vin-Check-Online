e5dcfc02943491e12b322b2eaff162a4898f7fbd
Dev <somanbandesha.sb@gmail.com>
2023-08-22 05:01:57 +0400

    Google Analytics
    google-analytics script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Footer, Navbar } from '@/components'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Vin Check Online',
@@ -13,6 +14,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-51GY603RL9" 
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-51GY603RL9', {cookie_flags: 'SameSite=None;Secure'});
+        `}
+      </Script>
       <body className="relative">
         <Navbar />
         {children}
